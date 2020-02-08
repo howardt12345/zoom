6576017d648977beb81f673d95ccc0e093c32e98
Howard Tseng <howardt12345@gmail.com>
2020-02-07 23:15:46 -0500

    update

---
+++ lib/components/classes.dart
@@ -0,0 +1,96 @@
+
+import 'package:flutter/material.dart';
+
+class Object {
+  String id;
+  Object(this.id);
+}
+
+class Profile extends Object {
+  String name, email, phone;
+  Profile({
+    this.name,
+    this.email,
+    this.phone,
+    @required String id
+  }) : super(id);
+}
+
+class Client extends Profile {
+  List<Item> favoriteItems;
+  List<String> addresses;
+  Client({
+    String name,
+    String email,
+    String phone,
+    this.favoriteItems,
+    this.addresses,
+  }) : super(
+    name: name,
+    email: email,
+    phone: phone,
+  );
+}
+
+class Driver extends Profile {
+  bool available;
+  double rating;
+  String licensePlate;
+  Driver({
+    String name,
+    String email,
+    String phone,
+    this.available,
+    this.rating,
+    this.licensePlate,
+  }) : super(
+    name: name,
+    email: email,
+    phone: phone,
+  );
+}
+
+class Store extends Profile {
+  String address;
+  double rating;
+  List<String> hours;
+  List<Item> items;
+  Store({
+    String name,
+    String email,
+    String phone,
+    this.address,
+    this.rating,
+    this.hours,
+    this.items,
+  }) : super(
+    name: name,
+    email: email,
+    phone: phone,
+  );
+}
+
+class Item extends Object {
+  String name, description;
+  double price, rating;
+  bool available;
+  Item({
+    this.name,
+    this.description,
+    this.price,
+    this.available,
+    @required String id,
+  }) : super(id);
+}
+
+class Order extends Object {
+  Client client;
+  Item item;
+  Driver driver;
+  Order({
+    this.client,
+    this.item,
+    this.driver,
+    @required String id,
+  }) : super(id);
+}
--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -3,11 +3,17 @@
 import 'package:flutter/material.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:zoom/ui/client/clientmain.dart';
+import 'package:zoom/ui/login/loginmain.dart';
+import 'package:zoom/ui/store/storemain.dart';
+
+import 'driver/drivermain.dart';
 
 enum PageState {
   Client,
   Driver,
   Store,
+  none,
 }
 
 class MainPage extends StatefulWidget {
@@ -20,17 +26,48 @@ class _MainPageState extends State<MainPage> {
 
   void initState() {
     super.initState();
-
   }
 
   Future<PageState> getPageState() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-
+    String state = prefs.getString("state") ?? '';
+    switch(state) {
+      case "client":
+        return PageState.Client;
+      case "driver":
+        return PageState.Driver;
+      case "store":
+        return PageState.Store;
+      default:
+        return PageState.none;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: getPageState(),
+      initialData: PageState.none,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          switch(snapshot.data) {
+            case PageState.Client:
+              return ClientPage();
+            case PageState.Driver:
+              return DriverPage();
+            case PageState.Store:
+              return StorePage();
+            case PageState.none:
+            default:
+              return LoginPage();
+          }
+        } else {
+          return Center(
+            child: Text("help plz there is error oh no"),
+          );
+        }
+      },
+    );
   }
 }
 
